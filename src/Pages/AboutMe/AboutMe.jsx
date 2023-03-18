127da63fb023628e48c8696786766731b34db8f6
Raul Villar <raultheviking@gmail.com>
2023-03-18 12:40:43 +0100

    Added new content to About Me

--- src/Pages/AboutMe/AboutMe.jsx
+++ src/Pages/AboutMe/AboutMe.jsx
@@ -3,12 +3,15 @@ import BgVideo from "../../Components/BgVideo/BgVideo";
 import Footer from "../../Components/Footer/Footer";
 import Header from "../../Components/Header/Header";
 import Grid from '@mui/material/Unstable_Grid2';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 
 
 export default function AboutMe() {
-
 
   return (
 
@@ -20,9 +23,9 @@ export default function AboutMe() {
       <Grid justifyContent="center" container >
         <Grid sx={{ bgcolor: 'rgba(84, 0, 0, 0.83)', borderRadius: '50px', p: 4 }} xs={10}>
           <Typography variant="h4" style={{ textAlign: 'center', color: "yellow" }}>About me</Typography >
-          <Typography style={{ fontFamily: 'poppins', fontSize: '1.1 rem', color: "white", paddingTop: '20px' }}>My name is Raúl Villar, I was born and raised in Asturias, I started at a young age to
+          <Typography style={{ fontFamily: 'poppins', fontSize: '1.1 rem', color: "white", paddingTop: '20px' }}>My name is Raúl Villar, I was born and raised in Asturias, at a young age I started to
             develop a genuinely curious and calmed personality, always in seek of knowledge, eager to learn new things. After I finished my bachelor’s degree in history, I decided to study web
-            programming. In the first moment, I was a bit hesitant, nonetheless, I discovered how much I enjoyed working in team, learning from my colleagues, and having fun while exploring new
+            programming. At first, I was a bit hesitant, nonetheless, I discovered how much I enjoyed working in team, learning from my colleagues, and having fun while exploring new
             paths -always applying the active learning pedagogy and agile methodology principles-. <br></br><br></br>
             I am immensely interested in finding my first job experience in the IT’s world so I can continue expanding my understanding -discovering new tools and capabilities which allow me to
             improve as a programmer-. After having completed a wide variety of projects -both individually and in team-, where I have always applied the clean code and SOLID principles, I am totally
@@ -30,26 +33,48 @@ export default function AboutMe() {
           </Typography>
         </Grid>
       </Grid>
-      <Grid justifyContent="center" container spacing={2} columns={12} sx={{ mt: 10, gap: 10 }}>
-        <Grid sx={{ bgcolor: 'rgba(84, 0, 0, 0.83)', borderRadius: '50px', }} xs={3}>
-          <Typography variant="h6" style={{ textAlign: 'center', color: "yellow" }}>Stack</Typography>
-          <Typography style={{ listStyle: 'none', textAlign: 'center', fontFamily: 'poppins', fontSize: '1.1 rem', color: "white", paddingTop: '10px' }}>
-            <li style={{ fontFamily: 'poppins' }}>HTML5</li>
-            <li style={{ fontFamily: 'poppins' }}>CSS3</li>
-            <li style={{ fontFamily: 'poppins' }}>JavaScript</li>
-            <li style={{ fontFamily: 'poppins' }}>React (Axios, Jest, MUI, Bootstrap)</li>
-            <li style={{ fontFamily: 'poppins' }}>Java (Spring Boot, Thymeleaf, JUnit)</li>
-          </Typography>
+      <Grid justifyContent="center" container spacing={2} columns={18} sx={{ mt: 10, gap: 10 }}>
+        <Grid xs={3}>
+          <Accordion sx={{ bgcolor: 'rgba(84, 0, 0, 0.83)', borderRadius: '50px', }}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography variant="h6" style={{ textAlign: 'center', color: "yellow" }}>Stack</Typography>
+            </AccordionSummary>
+            <AccordionDetails style={{ display: 'flex', flexDirection: 'column' }}>
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="HTML5" src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" />
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="CSS3" src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white" />
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="JavaScript" src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" />
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="React" src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" />
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="Bootstrap" src="https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white" />
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="MUI" src="https://img.shields.io/badge/MUI-%230081CB.svg?style=for-the-badge&logo=mui&logoColor=white" />
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="Jest" src="https://img.shields.io/badge/-jest-%23C21325?style=for-the-badge&logo=jest&logoColor=white" />
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="Java" src="https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white" />
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="Spring Boot" src="https://img.shields.io/badge/Spring-6DB33F?style=for-the-badge&logo=spring&logoColor=white" />
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="Thymeleaf" src="https://img.shields.io/badge/Thymeleaf-%23005C0F.svg?style=for-the-badge&logo=Thymeleaf&logoColor=white" />
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="JUnit" src="https://img.shields.io/badge/Junit5-25A162?style=for-the-badge&logo=junit5&logoColor=white" />
+            </AccordionDetails>
+          </Accordion>
         </Grid>
-        <Grid sx={{ bgcolor: 'rgba(84, 0, 0, 0.83)', borderRadius: '50px' }} xs={3}>
-          <Typography variant="h6" style={{ textAlign: 'center', color: "yellow" }}>Tools</Typography>
-          <Typography style={{ listStyle: 'none', textAlign: 'center', fontFamily: 'poppins', fontSize: '1.1 rem', color: "white", paddingTop: '10px' }}>
-            <li style={{ fontFamily: 'poppins' }}>VSC</li>
-            <li style={{ fontFamily: 'poppins' }}>IntelliJ</li>
-            <li style={{ fontFamily: 'poppins' }}>GitHub</li>
-            <li style={{ fontFamily: 'poppins' }}>Jira</li>
-            <li style={{ fontFamily: 'poppins' }}>Figma</li>
-          </Typography>
+        <Grid xs={3}>
+          <Accordion sx={{ bgcolor: 'rgba(84, 0, 0, 0.83)', borderRadius: '50px', }}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography variant="h6" style={{ textAlign: 'center', color: "yellow" }}>Tools</Typography>
+            </AccordionSummary>
+            <AccordionDetails style={{ display: 'flex', flexDirection: 'column' }}>
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="Visual Studio Code" src="https://img.shields.io/badge/VSCode-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white" />
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="IntelliJ" src="https://img.shields.io/badge/IntelliJIDEA-000000.svg?style=for-the-badge&logo=intellij-idea&logoColor=white" />
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="GitHub" src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" />
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="Jira" src="https://img.shields.io/badge/jira-%230A0FFF.svg?style=for-the-badge&logo=jira&logoColor=white" />
+              <img style={{ marginBottom: '10px' }} height="28" width="104" alt="Figma" src="https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white" />
+            </AccordionDetails>
+          </Accordion>
         </Grid>
       </Grid>
       <Footer />
