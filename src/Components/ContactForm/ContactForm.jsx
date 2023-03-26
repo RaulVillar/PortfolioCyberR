a47df7ac62a455ced9aa2b51063ef43e357b8b6e
Raul Villar <raultheviking@gmail.com>
2023-03-26 16:54:55 +0200

    Fixed the responsiveness in contact's page

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -20,14 +20,13 @@ export default function ContactForm() {
                 sx={{ color: "white", bgcolor: 'rgba(84, 0, 0, 0.83)', ml: "5vw", mr: "5vw", borderRadius: '50px', height: 700 }}
             >
                 <Typography variant="h4" sx={{ color: "yellow", mt: -5 }} >Contact</Typography >
-                <Stack direction="column" spacing={4} sx={{ fontSize: '1.5rem' }}>
+                <Stack direction="column" spacing={4} >
                     <Stack sx={{ fontSize: '1.2rem', mx: 1, mt: 5 }} >Email:</Stack>
-                    <TextField sx={{ bgcolor: 'white', borderRadius: "10px" }}
-                        size="lg"
-                        variant="outlined"
+                    <TextField sx={{ bgcolor: 'white', borderRadius: "10px", width: "330px", fontSize: '1.2rem' }}
                         type="email"
                         name="email"
                         placeholder="Write your e-mail here"
+                        required
                     />
                 </Stack>
                 <Stack direction="column" spacing={4} >
@@ -37,9 +36,8 @@ export default function ContactForm() {
                         name="message"
                         id="message"
                         placeholder="Write your message here"
-                        required=""
-                        maxRows={4}
-                        style={{ fontFamily: 'poppins', width: 350, height: 200, borderRadius: '10px', fontSize: '1.2rem' }}
+                        required
+                        style={{ fontFamily: 'poppins', width: 330, height: 200, borderRadius: '10px', fontSize: '1.2rem', padding: "3px" }}
                     />
                 </Stack>
                 <button class='button accept-btn' style={{ fontFamily: 'poppins' }}>Submit</button>
