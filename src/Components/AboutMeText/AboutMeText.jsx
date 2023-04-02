8ab4e980c16f0f485739573968e9267b2982c5f8
Raul Villar <raultheviking@gmail.com>
2023-04-02 11:08:25 +0200

    Fixed language mistake

--- src/Components/AboutMeText/AboutMeText.jsx
+++ src/Components/AboutMeText/AboutMeText.jsx
@@ -10,7 +10,7 @@ export default function AboutMeText() {
         <Typography variant="h4" style={{ textAlign: 'center', color: "yellow" }}>About me</Typography >
         <Typography style={{ fontFamily: 'poppins', fontSize: '1.1 rem', color: "white", paddingTop: '20px' }}>My name is Raúl Villar, I was born and raised in Asturias, at a young age I started to
           develop a genuinely curious and calmed personality, always in seek of knowledge, eager to learn new things. After I finished my bachelor’s degree in history, I decided to study web
-          programming. At first, I was a bit hesitant, nonetheless, I discovered how much I enjoyed working in team, learning from my colleagues, and having fun while exploring new
+          programming. At first, I was a bit hesitant, nonetheless, I discovered how much I enjoyed working with a team, learning from my colleagues, and having fun while exploring new
           paths -always applying the active learning pedagogy and agile methodology principles-. <br></br><br></br>
           I am immensely interested in finding my first job experience in the IT’s world so I can continue expanding my understanding -discovering new tools and capabilities which allow me to
           improve as a programmer-. After having completed a wide variety of projects -both individually and in team-, where I have always applied the clean code and SOLID principles, I am totally
